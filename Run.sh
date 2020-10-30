480c51e596955a9e8f2ab915142d162a44712873
Christoffer A Traeen <christoffer.andersen@live.com>
2020-10-30 14:47:15 +0100

    Update run

--- Run.sh
+++ Run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker container rm -f recipe-heaven 
-docker run --network=recipe_heaven_backend -p 9080:9080 -p 9443:9443 --user 1000 --name recipe_heaven -v $(pwd)/target:/config/dropins -v $(pwd)/server:/opt/ol/wlp/output/defaultServer/ no.twct.recipeheaven/recipeheaven 
+docker container rm -f recipe_heaven 
+docker run --network=recipe_heaven_backend -p 9080:9080 -p 9443:9443 --user 1000 --name recipe_heaven -v $(pwd)/target:/config/dropins -v $(pwd)/server:/opt/ol/wlp/output/defaultServer/ no.twct.recipeheaven/appserver 
