2f5e6d39005d344d37904f5064e96b752d95cd6b
Christoffer A Traeen <christoffer.andersen@live.com>
2020-11-08 21:55:02 +0100

    Remove getter for group

--- src/main/java/no/twct/recipeheaven/user/entity/Group.java
+++ src/main/java/no/twct/recipeheaven/user/entity/Group.java
@@ -1,13 +1,9 @@
 package no.twct.recipeheaven.user.entity;
 
-import java.util.List;
+import lombok.Data;
 
-import javax.json.bind.annotation.JsonbTransient;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.JoinColumn;
 import javax.persistence.Table;
 
 /*
@@ -15,30 +11,23 @@ import javax.persistence.Table;
 	used for permissions, where each group has different permissions when 
 	accessing resources. 
 */
+@Data
 @Entity
 @Table(name = "Groups")
 public class Group {
 
-	public static final String USER_GROUP_NAME = "user";
-	public static final String ADMIN_GROUP_NAME = "admin";
-	public static final String[] GROUPS = { USER_GROUP_NAME, ADMIN_GROUP_NAME };
+    public static final String USER_GROUP_NAME = "user";
+    public static final String ADMIN_GROUP_NAME = "admin";
+    public static final String[] GROUPS = {USER_GROUP_NAME, ADMIN_GROUP_NAME};
 
-	@Id
-	String name;
+    @Id
+    String name;
 
-	@JsonbTransient
-	@ManyToMany
-	@JoinTable(name = "user_groups", joinColumns = @JoinColumn(name = "name", referencedColumnName = "name"), inverseJoinColumns = @JoinColumn(name = "email", referencedColumnName = "email"))
-	List<User> users;
+    public Group() {
+    }
 
-	public Group() {
-	}
+    public Group(String name) {
+        this.name = name;
+    }
 
-	public Group(String name) {
-		this.name = name;
-	}
-
-	public String getName() {
-		return this.name;
-	}
 }
