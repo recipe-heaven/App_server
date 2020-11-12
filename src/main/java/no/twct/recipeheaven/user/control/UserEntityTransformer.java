a8ae01ccf768ed2acad23bcb77b64140bf18cc8f
Christoffer A Traeen <christoffer.andersen@live.com>
2020-11-12 15:29:45 +0100

    Add user DTO

---
+++ src/main/java/no/twct/recipeheaven/user/control/UserEntityTransformer.java
@@ -0,0 +1,22 @@
+package no.twct.recipeheaven.user.control;
+
+import no.twct.recipeheaven.user.entity.User;
+import no.twct.recipeheaven.user.entity.UserDetailsDTO;
+
+/**
+ * Provides transformer methods to create user specific DTOs
+ */
+public class UserEntityTransformer {
+
+    /**
+     * Creates a UserDetailsDTO
+     *
+     * @param user the user to transform
+     */
+    public UserDetailsDTO createUserDetailsDTO(User user) {
+        var userDetails = new UserDetailsDTO();
+        userDetails.setId(user.getId());
+        userDetails.setUsername(user.getUsername());
+        return userDetails;
+    }
+}
