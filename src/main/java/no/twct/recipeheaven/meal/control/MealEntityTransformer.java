989984c83aa1bb34d887aac9794f4fb964f370d6
Christoffer A Traeen <christoffer.andersen@live.com>
2020-11-13 01:48:45 +0100

    Update meal entity transformer

--- src/main/java/no/twct/recipeheaven/meal/control/MealEntityTransformer.java
+++ src/main/java/no/twct/recipeheaven/meal/control/MealEntityTransformer.java
@@ -1,9 +1,13 @@
 package no.twct.recipeheaven.meal.control;
 
+import no.twct.recipeheaven.meal.entity.FullMealDTO;
 import no.twct.recipeheaven.meal.entity.Meal;
-import no.twct.recipeheaven.meal.entity.MinifiedMealDTO;
+import no.twct.recipeheaven.meal.entity.MealDTO;
+import no.twct.recipeheaven.meal.entity.SimpleMealDTO;
 import no.twct.recipeheaven.recipe.control.RecipeEntityTransformer;
-import no.twct.recipeheaven.recipe.entity.MinifiedRecipeDTO;
+import no.twct.recipeheaven.recipe.entity.FullRecipeDTO;
+import no.twct.recipeheaven.recipe.entity.RecipeDTO;
+import no.twct.recipeheaven.user.control.UserEntityTransformer;
 
 import javax.inject.Inject;
 import java.util.List;
@@ -18,17 +22,41 @@ public class MealEntityTransformer {
     @Inject
     RecipeEntityTransformer recipeEntityTransformer;
 
+    @Inject
+    UserEntityTransformer userEntityTransformer;
+
+    private void setBaseDtoValues(MealDTO dto, Meal meal) {
+        dto.setName(meal.getName());
+        dto.setOwner(userEntityTransformer.createUserDetailsDTO(meal.getCreator()));
+    }
+
     /**
      * Creates a minified projection of a meal entity, with all recipes minified as well.
      *
      * @param meal the meal to transform
      * @return returns the minified projection
      */
-    public MinifiedMealDTO createMealDetailsDTO(Meal meal) {
-        MinifiedMealDTO mealSimpleDetailsDTO = new MinifiedMealDTO();
-        mealSimpleDetailsDTO.setName(meal.getName());
-        mealSimpleDetailsDTO.setOwner(meal.getCreator().getId());
-        List<MinifiedRecipeDTO> recipes = meal.getRecipes().stream().map(recipeEntityTransformer::createSimpleRecipeDTO).collect(Collectors.toList());
+    public SimpleMealDTO createSimpleMealDTO(Meal meal) {
+        SimpleMealDTO mealSimpleDetailsDTO = new SimpleMealDTO();
+        setBaseDtoValues(mealSimpleDetailsDTO, meal);
+        List<RecipeDTO> recipes = meal.getMealRecipeTypes().stream()
+                .map((mealType) -> recipeEntityTransformer.createSimpleRecipeDTO(mealType.getRecipe())).collect(Collectors.toList());
+        mealSimpleDetailsDTO.setRecipes(recipes);
+        return mealSimpleDetailsDTO;
+    }
+
+
+    /**
+     * Creates a full projection of a meal entity, with all recipes details.
+     *
+     * @param meal the meal to transform
+     * @return returns the full projection
+     */
+    public FullMealDTO createFullMealDTO(Meal meal) {
+        FullMealDTO mealSimpleDetailsDTO = new FullMealDTO();
+        setBaseDtoValues(mealSimpleDetailsDTO, meal);
+        List<FullRecipeDTO> recipes = meal.getMealRecipeTypes().stream()
+                .map((mealType) -> recipeEntityTransformer.createFullRecipeDTO(mealType.getRecipe())).collect(Collectors.toList());
         mealSimpleDetailsDTO.setRecipes(recipes);
         return mealSimpleDetailsDTO;
     }
