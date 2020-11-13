3d78d7431a04841b3040dd5c9edeb02386a04933
Christoffer A Traeen <christoffer.andersen@live.com>
2020-11-13 01:49:14 +0100

    Add meal recipe types

--- src/main/java/no/twct/recipeheaven/Const.java
+++ src/main/java/no/twct/recipeheaven/Const.java
@@ -8,5 +8,9 @@ public final class Const {
     public static final String MEAL_TYPE_NAME = "meal";
     public static final String MENU_TYPE_NAME = "menu";
 
+    public static final String MEAL_STARTER_NAME = "starter";
+    public static final String MEAL_COURSE_NAME = "course";
+    public static final String MEAL_DESSERT_NAME = "dessert";
+
     public static final String MENU_TYPE_NAMEksjd = "menu";
 }
