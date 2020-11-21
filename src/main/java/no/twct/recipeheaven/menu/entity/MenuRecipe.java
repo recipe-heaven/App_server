93682e5c68cac6a01eec0abc5fe624291deb069b
Christoffer A Traeen <christoffer.andersen@live.com>
2020-11-21 21:03:25 +0100

    Add call super for subclass

--- src/main/java/no/twct/recipeheaven/menu/entity/MenuRecipe.java
+++ src/main/java/no/twct/recipeheaven/menu/entity/MenuRecipe.java
@@ -1,21 +1,23 @@
 package no.twct.recipeheaven.menu.entity;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import no.twct.recipeheaven.recipe.entity.Recipe;
 
-import javax.persistence.*;
-import java.math.BigInteger;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Data
 @Entity
 @NoArgsConstructor
 @Table(name = "menu_recipes")
+@EqualsAndHashCode(callSuper = true)
 public class MenuRecipe extends MenuItem {
 
     @OneToOne
     private Recipe recipe;
-
 
     @Override
     public ItemTypes getItemType() {
