7fdb7062bc4a3132cd0aeb11b9e9d0f101f0610c
Christoffer A Traeen <christoffer.andersen@live.com>
2020-11-21 21:02:18 +0100

    Add validator to menu

--- src/main/java/no/twct/recipeheaven/menu/entity/Menu.java
+++ src/main/java/no/twct/recipeheaven/menu/entity/Menu.java
@@ -5,12 +5,12 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import no.twct.recipeheaven.lib.CreatableBase;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,27 +21,26 @@ import java.util.stream.Collectors;
 @EqualsAndHashCode(callSuper = true)
 public class Menu extends CreatableBase {
 
+    @NotEmpty
     String name;
 
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
-    List<MenuItem> menuItems;
-
-    // i am tiered this is temporary
+    List<@Valid MenuItem> menuItems;
 
     public List<MenuRecipe> getRecipes() {
         return menuItems.stream()
-                        .filter(menuItem -> menuItem.getItemType().equals(MenuItem.ItemTypes.recipe))
-                        .map(menuItem -> (MenuRecipe) menuItem)
-                        .collect(
-                                Collectors.toList());
+                .filter(menuItem -> menuItem.getItemType().equals(MenuItem.ItemTypes.recipe))
+                .map(menuItem -> (MenuRecipe) menuItem)
+                .collect(
+                        Collectors.toList());
     }
 
     public List<MenuMeal> getMeals() {
         return menuItems.stream()
-                        .filter(menuItem -> menuItem.getItemType().equals(MenuItem.ItemTypes.meal))
-                        .map(menuItem -> (MenuMeal) menuItem)
-                        .collect(
-                                Collectors.toList());
+                .filter(menuItem -> menuItem.getItemType().equals(MenuItem.ItemTypes.meal))
+                .map(menuItem -> (MenuMeal) menuItem)
+                .collect(
+                        Collectors.toList());
     }
 
 }
