6cdeb510eca9f8a7076437cba6ca55215fdd417b
trygve <trygve.woldseth@gmail.com>
2020-11-22 11:21:27 +0100

    rename

--- src/main/java/no/twct/recipeheaven/resources/entity/UserMetaInfo.java
+++ src/main/java/no/twct/recipeheaven/resources/entity/UserMetaInfo.java
@@ -6,21 +6,17 @@ import no.twct.recipeheaven.lib.CreatableBase;
 import no.twct.recipeheaven.menu.entity.Menu;
 import no.twct.recipeheaven.user.entity.User;
 
-import javax.json.bind.annotation.JsonbTypeAdapter;
-import javax.json.bind.annotation.JsonbTypeSerializer;
-
 import javax.persistence.*;
 import java.io.Serializable;
 import java.math.BigInteger;
 import java.util.List;
-import java.util.Queue;
 
 @Entity
 @Data
 @NoArgsConstructor
 @Table(name = "UserStatus")
-@NamedQuery(name = "UserStatus.GetByOwnerId", query = "select s from UserStatus s where s.owner.id = :id")
-public class UserStatus implements Serializable {
+@NamedQuery(name = "UserStatus.GetByOwnerId", query = "select s from UserMetaInfo s where s.owner.id = :id")
+public class UserMetaInfo implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private BigInteger id;
@@ -35,13 +31,13 @@ public class UserStatus implements Serializable {
     @ManyToMany(cascade = CascadeType.ALL)
     private List<CreatableBase> staredEntities;
 
-    public UserStatus(User owner) {
+    public UserMetaInfo(User owner) {
         this.owner = owner;
         this.id = owner.getId();
     }
 
-    public static UserStatus getByUserId(EntityManager entityManager, BigInteger userId){
-        Query query = entityManager.createNamedQuery("UserStatus.GetByOwnerId", UserStatus.class).setParameter("id",userId );
-        return (UserStatus) query.getSingleResult();
+    public static UserMetaInfo getByUserId(EntityManager entityManager, BigInteger userId){
+        Query query = entityManager.createNamedQuery("UserStatus.GetByOwnerId", UserMetaInfo.class).setParameter("id", userId );
+        return (UserMetaInfo) query.getSingleResult();
     }
 }
